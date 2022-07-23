55e0a58621459bc79faf4b6de93b0717f2ce0cb6
JakaV08 <109280681+JakaV08@users.noreply.github.com>
2022-07-23 22:30:34 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -24,29 +24,29 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
+global.botname = "AngelChan-Mdོ" //namabot kalian
+global.ownername= "ᴹᴿ᭄ JkzOfcོ ×፝֟͜×" //nama kalian
+global.myweb ="https://Instagram.com/nan_sakrn" //bebas asal jan hapus
 global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
 global.github = "https://zeeone-ofc.github.io/" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
-global.region = "Indonesia" //bebas
+global.email = "aknokteto2@gmail.com" //bebas
+global.region = "United states" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["622150996855","6285342106390"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
-global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
+global.premium = ['19793006026'] //premium user
+global.owner = ["19793006026","19793006026"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "19793006026" // nomor wa kalian
+global.ownernomerr = "+19793006026" //nmr wa kalian
+global.thumbnail = "./image/lold.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = '© AngelChan-Mdོ' //sticker wm ubah
+global.author = 'Di Buat Oleh JkzOfc' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 
 global.limitawal = {
     premium: "Infinity",
-    free: 100
+    free: 63
 }
 
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
